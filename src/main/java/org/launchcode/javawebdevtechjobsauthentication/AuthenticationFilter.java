11a51408691bed690469308d67b597ba15a914e7
Polly Wong <spwong.polly@gmail.com>
2020-11-04 18:41:41 -0600

    Studio 19 complete

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/AuthenticationFilter.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/AuthenticationFilter.java
@@ -37,6 +37,12 @@ public class AuthenticationFilter extends HandlerInterceptorAdapter {
                              HttpServletResponse response,
                              Object handler) throws IOException {
 
+        // Don't require sign-in for whitelisted pages
+        if (isWhitelisted(request.getRequestURI())) {
+            // returning true indicates that the request may proceed
+            return true;
+        }
+
         HttpSession session = request.getSession();
         User user = authenticationController.getUserFromSession(session);
 
